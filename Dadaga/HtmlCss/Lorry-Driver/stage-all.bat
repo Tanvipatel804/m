7cccc781a919d22a58d939d770966e6e29f58fb2
dadga <dadgapatel88@gmail.com>
2026-03-23 11:59:52 +0400

    git add -A

---
+++ Dadaga/HtmlCss/Lorry-Driver/stage-all.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "c:\Users\DADAGA\Desktop\Dadaga\HtmlCss\Lorry-Driver"
+git add -A
+echo All files staged automatically!
---
+++ Dadaga/HtmlCss/Lorry-Driver/stage-all.ps1
@@ -0,0 +1,3 @@
+﻿Set-Location "c:\Users\DADAGA\Desktop\Dadaga\HtmlCss\Lorry-Driver"
+git add -A
+Write-Host "All files staged automatically!" -ForegroundColor Green
